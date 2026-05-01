23d5479008482a2a974697fb92872c6341d20340
adk290905 <rouhehessoufeye@gmail.com>
2026-05-01 21:22:57 +0200

    fin partie 5

--- index.php
+++ index.php
@@ -4,22 +4,20 @@
 ?>
 
 
-
-
-<style>
-    form { display: flex; flex-direction: column; gap: 10px; }
-</style>
 <form method = "POST" action = "traitement.php">
+	<center><h2>Inscription</h2></center>
 	<input type = "hidden" name = "key" value = "<?=$id ?>">
 	<input type="text" placeholder = "votre nom" name = "nomE" value = "<?=$nom_etu ?>">
 	<input type = "text" placeholder = "votre prenom" name = "preE" value = "<?=$prenom_etu ?>" >
-	<input type="submit" name ="btn" value="Ajouter">
+	
 	<select>
+		<option>Choisissez une option</option>
 		<option>SIL</option>
 		<option>SI</option>
 		<option>RIT</option>
 		<option>ER</option>
 	</select>
+	<input type="submit" name ="btn" value="Ajouter">
 	
 </form>
 
